3135f033d24befa9fd8657e58bd6ed71c6ab75a1
Observer <617710664@qq.com>
2023-06-26 20:24:27 +0800

    finish hw08

--- hw08/hw08.scm
+++ hw08/hw08.scm
@@ -1,8 +1,29 @@
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s) 
+  (cond
+    ((null? s) s)
+    ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+    (else (my-filter pred (cdr s)))
+  )
+)
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+  (cond 
+    ((null? lst1) lst2)
+    ((null? lst2) lst1)
+    (else (append (append (list (car lst1)) (list (car lst2))) (interleave (cdr lst1) (cdr lst2))))
+  )
+)
 
 (define (accumulate joiner start n term)
-  'YOUR-CODE-HERE)
+  (cond 
+    ((= n 0) start)
+    (else (joiner (term n) (accumulate joiner start (- n 1) term)))
+  )
+)
 
-(define (no-repeats lst) 'YOUR-CODE-HERE)
+(define (no-repeats lst) 
+  (cond
+    ((null? lst) lst)
+    (else (append (list (car lst)) (no-repeats (my-filter (lambda (f) (not (= f (car lst)))) (cdr lst)))))
+  )
+)
