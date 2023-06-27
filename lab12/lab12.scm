0755162ed133cd456ebe585501027445362d475f
Observer <617710664@qq.com>
2023-06-27 21:54:47 +0800

    finish lab12

--- lab12/lab12.scm
+++ lab12/lab12.scm
@@ -1,26 +1,36 @@
 (define-macro
  (if-macro condition if-true if-false)
- 'YOUR-CODE-HERE)
+ `(if ,condition ,if-true ,if-false)
+)
 
 (define-macro (or-macro expr1 expr2)
-  `(let ((v1 ____________))
-     (if _____
-         _____
-         _____)))
+  `(let ((v1 ,expr1))
+     (if v1
+         v1
+         ,expr2)))
 
-(define (replicate x n) 'YOUR-CODE-HERE)
+(define (replicate x n) 
+  (cond
+    ((= n 0) nil)
+    (else (append (list x) (replicate x (- n 1))))
+  )
+)
 
-(define-macro (repeat-n expr n) 'YOUR-CODE-HERE)
+(define-macro (repeat-n expr n) 
+  `(cond
+    ((= ,n 0) undefined)
+    (else (begin
+      ,expr
+      (repeat-n ,expr (- ,n 1))
+    ))
+  )
+)
 
 (define
  (list-of map-expr for var in lst if filter-expr)
- 'YOUR-CODE-HERE)
+ `(map (lambda (,var) ,map-expr) (filter (lambda (,var) ,filter-expr) ,lst))
+ )
 
-(define-macro (list-of-macro map-expr
-                             for
-                             var
-                             in
-                             lst
-                             if
-                             filter-expr)
-  'YOUR-CODE-HERE)
+(define-macro (list-of-macro map-expr for var in lst if filter-expr)
+  `(map (lambda (,var) ,map-expr) (filter (lambda (,var) ,filter-expr) ,lst))
+)
